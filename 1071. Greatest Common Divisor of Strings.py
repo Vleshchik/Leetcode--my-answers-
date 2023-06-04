d897736a1322dc74773b9981586427cff362aa8a
Leshchik_Vadim <vleshchik@gmail.com>
2023-06-04 14:01:55 +0300

    my answer for leetcode tasks

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -1,10 +1,11 @@
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        s = []
-        max_s = word1 if len(word1) > len(word2) else word2
-        for i in range(min(len(word1), len(word2))):
-            s.append(word1[i])
-            s.append(word2[i])
-            max_s = max_s[1:]
-        s.append(max_s)
-        return ''.join(s)
+    def gcdOfStrings(self, s: str, t: str) -> str:
+        x = ''
+        for i in range(len(t), 0, -1):
+            if t[0:i] in s:
+                x = t[0:i]
+                if len(s.replace(x, ''))== 0 and len(t.replace(x, ''))== 0:
+                    break
+                else:
+                    x = ''
+        return x
