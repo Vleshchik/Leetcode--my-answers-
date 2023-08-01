c63d8eabd993f917588964a942cf7980a6784749
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-01 20:15:25 +0300

    my answer for leetcode problem

---
+++ 77. Combinations.py
@@ -0,0 +1,13 @@
+lass Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        l = []
+        def a (s, l2):
+            if len(l2) == k:
+                l.append(l2.copy())
+                return
+            for i in range(s, n + 1):
+                l2.append(i)
+                a(i+1, l2)
+                l2.pop()
+        a(1,[])
+        return l
