52c39ead29801ec1472cfb9427c1bef1e1e98859
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-07 17:23:35 +0300

    my answer for leetcode problem

--- 1683. Invalid Tweets.py
+++ 1683. Invalid Tweets.py
@@ -1,5 +1,7 @@
-import pandas as pd
+# Write your MySQL query statement below
+# select tweet_id from tweets where length(content) > 15
 
+import pandas as pd
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     mask = tweets['content'].str.len() > 15
     return tweets[mask][['tweet_id']]
