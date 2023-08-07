ca3bab794a4789d9282d43f19983cfe35d0e90ef
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-07 17:29:30 +0300

    my answer for leetcode problem

--- 1148. Article Views I.py
+++ 1148. Article Views I.py
@@ -1,4 +1,6 @@
-import pandas as pd
+# Write your MySQL query statement below
+select distinct author_id as id from views where author_id = viewer_id order by id ASC
 
+import pandas as pd
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
     return views[views['author_id'] == views['viewer_id']][['author_id']].drop_duplicates().rename(columns={'author_id': 'id'}).sort_values(by='id', ascending=True)
