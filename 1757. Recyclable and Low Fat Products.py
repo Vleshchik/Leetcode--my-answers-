d9cd2163d5f4524e16ec249837b22039ba9b17c8
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-07 17:14:24 +0300

    my answer for leetcode problem

--- 1757. Recyclable and Low Fat Products.py
+++ 1757. Recyclable and Low Fat Products.py
@@ -1,5 +1,8 @@
-import pandas as pd
+# Write your MySQL query statement below
+# select product_id from products where low_fats = 'Y' and recyclable = 'Y'
 
+
+import pandas as pd
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     mask = (products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')
     return products[mask][['product_id']]
