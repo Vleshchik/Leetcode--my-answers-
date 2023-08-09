9a6c8791ec33d5263c94519a6ac3739ed31113cb
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-09 19:38:38 +0300

    my answer for leetcode problem

--- 1280. Students and Examinations.py
+++ 1280. Students and Examinations.py
@@ -5,3 +5,4 @@ CROSS JOIN Subjects su
 LEFT JOIN Examinations e ON s.student_id = e.student_id AND su.subject_name = e.subject_name
 GROUP BY s.student_id, s.student_name, su.subject_name
 ORDER BY s.student_id, su.subject_name;
+
