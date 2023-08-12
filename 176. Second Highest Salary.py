d26ed73b7724c522a3fc7c0e947e4d81e6357ba7
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-12 19:10:50 +0300

    my answer for leetcode problem

---
+++ 176. Second Highest Salary.py
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    salary_df = employee['salary'].sort_values(ascending=False).drop_duplicates()
+    return pd.DataFrame({'SecondHighestSalary':[salary_df.iloc[1]]}) if len(salary_df) > 1 else pd.DataFrame({'SecondHighestSalary':[None]})
