30b221662a9785c8743657f8d08bd50e913d7366
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-15 20:34:13 +0300

    my answer for leetcode problem

--- 1378. Replace Employee ID With The Unique Identifier.py
+++ 1378. Replace Employee ID With The Unique Identifier.py
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-SELECT
-    CASE WHEN eu.unique_id IS NOT NULL THEN eu.unique_id ELSE NULL END AS unique_id,
-    e.name
+'''SELECT CASE
+WHEN eu.unique_id IS NOT NULL
+THEN eu.unique_id ELSE NULL END AS unique_id, e.name
 FROM Employees e
-LEFT JOIN EmployeeUNI eu ON e.id = eu.id;
+LEFT JOIN EmployeeUNI eu ON e.id = eu.id;'''
+
+import pandas as pd
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    df=pd.merge(employees,employee_uni,how='left',on='id')
+    return df[['unique_id','name']]
