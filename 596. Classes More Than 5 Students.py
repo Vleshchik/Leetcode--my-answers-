2980da942c531286855606fd254008a9f4d4b729
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-16 22:08:44 +0300

    my answer for leetcode problem

--- 596. Classes More Than 5 Students.py
+++ 596. Classes More Than 5 Students.py
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-SELECT class FROM Courses GROUP BY class HAVING COUNT(student) >= 5;
+'''SELECT class FROM Courses GROUP BY class HAVING COUNT(student) >= 5;'''
+
+import pandas as pd
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    class_counts = courses.groupby('class')['student'].count().reset_index()
+    result = class_counts[class_counts['student'] >= 5][["class"]]
+    return result
