97fc33e84e6722f2a38178e1266df7913fad04c4
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-19 18:58:49 +0300

    my answer for leetcode problem

---
+++ 2806. Account Balance After Rounded Purchase.py
@@ -0,0 +1,6 @@
+class Solution:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
+        if purchaseAmount % 10 < 5:
+            return 100 - (purchaseAmount // 10 * 10)
+        else:
+            return 100 - ((purchaseAmount // 10 + 1) * 10)
