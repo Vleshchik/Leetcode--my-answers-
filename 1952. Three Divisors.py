56355e10b0e9cb62f9c7d0db186a2f8a1a87670a
Leshchik_Vadim <vleshchik@gmail.com>
2023-09-04 21:57:59 +0300

    my answer for leetcode problem

---
+++ 1952. Three Divisors.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        c = 1
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                c += 1
+            if c > 3:
+                return False
+        return True if c == 3 else False
